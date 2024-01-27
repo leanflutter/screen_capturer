3a3bf32d9dc7002db88724820922119a195c352b
LiJianying <lijy91@foxmail.com>
2024-01-27 14:38:14 +0800

    wip: remove exmaple hotkey_manager lib

--- packages/screen_capturer/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/screen_capturer/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <hotkey_manager/hotkey_manager_plugin.h>
 #include <screen_capturer_windows/screen_capturer_windows_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  HotkeyManagerPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("HotkeyManagerPlugin"));
   ScreenCapturerWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ScreenCapturerWindowsPluginCApi"));
 }
